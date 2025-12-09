13b9bb461dd4da8acb383c181e9cea1612bc9c1d
TenZees <TenZees.Creations@protonmail.com>
2025-12-09 05:10:32 -0600

    Update site

---
+++ generator/general.js
@@ -0,0 +1,7 @@
+
+  export const generalPrompts = [
+        "A traveler who collects emotions in glass bottles.",
+        "A creature made of stitched-together constellations.",
+        "A forgotten door in your house that leads somewhere impossible.",
+        "Someone who can speak to broken machines.",
+    ];
